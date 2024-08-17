37aad21a56946e5c21f05a32cfd82c5e328d7147
Loc Le <lpl212757@gmail.com>
2024-08-17 20:28:52 +0700

    feat - implement dynamic title

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,12 @@
+import DynamicTitle from '../components/shared/DynamicTitle';
+
+function Blog() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+        <DynamicTitle title="Simple Resume - Blog"/>
+      <p className="text-lg mt-4">Blog</p>
+    </div>
+  )
+}
+
+export default Blog;
